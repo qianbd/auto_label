9ad2381fa48b1602d4da84e566fc4f22d6a16e4b
JiaweiGu <42446055+qianbd@users.noreply.github.com>
2022-08-15 17:46:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from github import Github
 from requests import delete
 
 gh_url = "https://github.com"
-token = os.environ['GH_PAT']
+token = os.environ["GH_PAT"]
 gh = Github(token)
-issue_labels=os.environ['ISSUE_LABEL']
-pr_labels=os.environ['PR_LABEL']
+issue_labels = os.environ["ISSUE_LABEL"]
+pr_labels = os.environ["PR_LABEL"]
 
 
 def get_yesterday_time():
@@ -18,7 +18,7 @@ def get_yesterday_time():
 
 def check_element_label(element, label):
     for i in element.labels:
-        li=i.name.split('/')
+        li = i.name.split("/")
         if label in li:
             return True
     return False
@@ -26,7 +26,7 @@ def check_element_label(element, label):
 
 def get_issues(repo):
     list_issue = []
-    for type_state in ['open','closed']:
+    for type_state in ["open", "closed"]:
         for issue in repo.get_issues(state=type_state):
             if not issue.pull_request:
                 list_issue.append(issue)
@@ -45,7 +45,7 @@ def get_prs(repo):
 
 
 def mod_pr_issue_label(list_element, label):
-    none_num =  0
+    none_num = 0
     label_num = 0
     for element in list_element:
         if check_element_label(element, label) == False:
@@ -59,19 +59,18 @@ def mod_pr_issue_label(list_element, label):
                 label_num = 1
 
     if none_num == 1 and label_num == 1:
-        element.remove_labels("{}/none".format(label))                
-        
+        element.remove_labels("{}/none".format(label))
 
 
-def main(repo_name,issue_num,pr_num):
-    list_issue_label=issue_labels.split(';')
-    list_pr_label=pr_labels.split(';')
+def main(repo_name, issue_num, pr_num):
+    list_issue_label = issue_labels.split(";")
+    list_pr_label = pr_labels.split(";")
     repo = gh.get_repo(repo_name)
 
     if issue_num:
         list_issues = repo.get_issue(issue_num)
     else:
-        list_issues = get_issues(repo)  
+        list_issues = get_issues(repo)
     for each in list_issue_label:
         mod_pr_issue_label(list_issues, each)
 
@@ -79,13 +78,18 @@ def main(repo_name,issue_num,pr_num):
         list_prs = repo.get_pull(pr_num)
     else:
         list_prs = get_prs(repo)
-    
+
     for each in list_pr_label:
-        mod_pr_issue_label(list_prs,each)
+        mod_pr_issue_label(list_prs, each)
+
 
 if __name__ == "__main__":
-    repo_name = os.environ["GITHUB_REPOSITORY"] 
-    issue_num = int(float(os.environ["IU_NUM"]))
-    pr_num    = int(float(os.environ["PR_NUM"]))
-    print(">>> issue number: {}, pr number: {}", format(issue_num), format(pr_num))
-    main(repo_name,issue_num,pr_num)
+    repo_name = os.environ["GITHUB_REPOSITORY"]
+    issue_num = os.environ["IU_NUM"]
+    pr_num = os.environ["PR_NUM"]
+    if issue_num == None:
+        issue_num = ""
+    if pr_num == None:
+        pr_num = ""
+    print(">>> issue number: {}, pr number: {}".format(issue_num, pr_num))
+    main(repo_name, int(issue_num), (pr_num))
