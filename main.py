3aad457e8000a41ffe1ad54f2f629e7061e1faf1
sophie-xie <84560950+Sophie-Xie@users.noreply.github.com>
2022-08-15 15:10:33 +0800

    event trigger

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from datetime import datetime, timedelta
 from github import Github
+from requests import delete
 
 gh_url = "https://github.com"
 token = os.environ['GH_PAT']
@@ -43,24 +44,45 @@ def get_prs(repo):
     return list_pr
 
 
-def add_pr_issue_label(list_element, label):
+def mod_pr_issue_label(list_element, label):
     for element in list_element:
         if check_element_label(element, label) == False:
             # element.set_labels(*element.labels, "{}/none".format(label))
             element.add_to_labels("{}/none".format(label))
+        else:
+            none_label = "{}/none".format(label)
+            if check_element_label(element, none_label) == True:
+                none_num = 1
+            else:
+                label_num = 1
+
+    if none_num == 1 and label_num == 1:
+        element.remove_labels("{}/none".format(label))                
+        
 
 
-def main(repo_name):
-    repo = gh.get_repo(repo_name)
-    list_issues = get_issues(repo)
-    list_prs = get_prs(repo)
+def main(repo_name,issue_num,pr_num):
     list_issue_label=issue_labels.split(';')
-    for each in list_issue_label:
-        add_pr_issue_label(list_issues, each)
     list_pr_label=pr_labels.split(';')
+    repo = gh.get_repo(repo_name)
+
+    if issue_num:
+        list_issues = repo.get_issue(issue_num)
+    else:
+        list_issues = get_issues(repo)  
+    for each in list_issue_label:
+        mod_pr_issue_label(list_issues, each)
+
+    if pr_num:
+        list_prs = repo.get_pull(pr_num)
+    else:
+        list_prs = get_prs(repo)
+    
     for each in list_pr_label:
-        add_pr_issue_label(list_prs,each)
+        mod_pr_issue_label(list_prs,each)
 
 if __name__ == "__main__":
     repo_name = os.environ["GITHUB_REPOSITORY"] 
-    main(repo_name)
+    issue_num = os.environ["INPUT_IU_NUM"]
+    pr_num    = os.environ["INPUT_PR_NUM"]
+    main(repo_name,issue_num,pr_num)
